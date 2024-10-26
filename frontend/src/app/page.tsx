be2c36c1f230cdb0453f6e4503d9210ae764ef71
nia <niabowrin@gmail.com>
2024-10-26 18:45:07 -0400

    buttons and file upload

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,26 @@
+"use client"; 
 
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './page.module.css';
 
 const Page: React.FC = () => {
+  const [fileUploaded, setFileUploaded] = useState(false);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      // Perform file validation or parsing here if needed
+      console.log("File uploaded:", file.name);
+      setFileUploaded(true);
+    }
+  };
+
   return (
     <div>
       <div className={styles.container}>
         <h1 className={styles.title}>PatientView.io</h1>
       </div>
+      
       
       <div className={styles.container1}>
         <h2 className={styles.title}>Graph</h2>
@@ -15,8 +28,21 @@ const Page: React.FC = () => {
       </div>
 
       <div className={styles.container2}>
-        <h2 className={styles.title}>Inputs</h2>
-        <p></p>
+        <p>Upload your json file:</p>
+        <input 
+          type="file"
+          accept=".json"
+          className={styles.fileInput}
+          onChange={handleFileChange}
+        />  
+        {fileUploaded && (
+        <div className={styles.buttonContainer}>
+          <button className={styles.roundButton}>Button 1</button>
+          <button className={styles.roundButton}>Button 2</button>
+          <button className={styles.roundButton}>Button 3</button>
+          <button className={styles.roundButton}>Button 4</button>
+        </div>
+        )}
       </div>
     </div>
   );
