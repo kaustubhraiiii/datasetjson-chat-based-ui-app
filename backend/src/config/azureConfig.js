c2eeb9543a15aa84cc2cfcffef03e43e12940fa1
Kaustubh Rai <hello@kaustbhrai.com>
2024-10-26 16:16:11 -0400

    changed azure config

---
+++ backend/src/config/azureConfig.js
@@ -0,0 +1,33 @@
+const sql = require('mssql');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+
+const sqlConfig = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    server: process.env.DB_SERVER,
+    options: {
+        encrypt: true,
+        enableArithAbort: true
+    }
+};
+
+const poolPromise = new sql.ConnectionPool(sqlConfig)
+    .connect()
+    .then(pool => {
+        console.log('Connected to Azure SQL');
+        return pool;
+    })
+    .catch(err => {
+        console.error('Database connection failed: ', err);
+        process.exit(1);
+    });
+
+module.exports = {
+    sql,
+    poolPromise
+};
+
